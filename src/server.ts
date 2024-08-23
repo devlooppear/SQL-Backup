5198d8c9dda5782338ae8fbaab4204d08ab2d866
devlooppear <iago.profissional.developer@gmail.com>
2024-08-23 19:11:13 -0300

    feat: actions just to pipeline ci cd

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,11 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import cron from 'node-cron';
-import { exec } from 'child_process';
-import util from 'util';
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
-
-// Define the schedule for the cron job using the format 'minute hour day-of-month month day-of-week'
-// The format is:
-// ────────────── minute (0-59)
-// ────────────── hour (0-23)
-// ────────────── day-of-month (1-31)
-// ────────────── month (1-12)
-// ────────────── day-of-week (0-6) (0 is Sunday, 1 is Monday, etc.)
-
-// Example: '0 13 * * *' means:
-// - '0' minute: The job will run at the 0th minute (the start of the hour)
-// - '13' hour: The job will run at 13:00 (1 PM in 24-hour format)
-// - '*' day-of-month: The job will run on any day of the month
-// - '*' month: The job will run in any month
-// - '*' day-of-week: The job will run on any day of the week
-
-// Therefore, '0 13 * * *' configures the cron job to run daily at 13:00 (1 PM).
-
-const BACKUP_SCHEDULE = '0 13 * * *';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
@@ -90,15 +68,4 @@ app.get('/api/candies', async (req, res) => {
 app.listen(port, async () => {
   console.log(`Server running at http://localhost:${port}`);
   await setupDatabase();
-});
-
-const execPromise = util.promisify(exec);
-cron.schedule(BACKUP_SCHEDULE, async () => {
-  try {
-    console.log('Starting daily backup...');
-    await execPromise('./backup.sh');
-    console.log('Backup completed successfully.');
-  } catch (error) {
-    console.error('Error during backup:', error);
-  }
 });
